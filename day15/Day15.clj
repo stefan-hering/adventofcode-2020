ec0e3bb195aae944e1bc9ff72af1361ae9afecc6
Stefan Hering <stefan.hering@rewe-digital.com>
2021-01-07 17:37:38 +0100

    day 15 cleanup

--- day15/Day15.clj
+++ day15/Day15.clj
@@ -6,26 +6,23 @@
    9  5
    1  6})
 
-(declare nextNumber)
-
-(defn calculateNumber [indexes, i, prevnum, isfirst, target]
+(defn calculateNumber [indexes, i, prevnum, isfirst nextNumber]
   (let [index   (get indexes prevnum -1)
         current (if isfirst 0 (- i index 1))]
 
     (nextNumber (conj indexes {prevnum (- i 1)})
-                   i
-                   current
-                   (not (contains? indexes current))
-                   target)))
+              i
+              current
+              (not (contains? indexes current)))))
 
-(defn nextNumber [indexes, i, current, isfirst, target]
-  #(if (= i target) current (calculateNumber indexes (+ i 1) current isfirst target)))
-
+(defn nextNumber [target, indexes, i, current, isfirst]
+  (let [nextNumber (partial nextNumber target)]
+    #(if (= i target)
+      current
+      (calculateNumber indexes (+ i 1) current isfirst nextNumber))))
 
 (println "part1")
-(println (trampoline nextNumber input 6 1 true 2020))
-
+(println (trampoline nextNumber 2020 input 6 1 true))
 
 (println "part2")
-(println (trampoline nextNumber input 6 1 true 30000000))
-
+(println (trampoline nextNumber 30000000 input 6 1 true))
